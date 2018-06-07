f95c9b4d5e2dfdb87a7b30ab26b90225891cec6e
Mathias Kaufmann <me@stei.gr>
2018-06-07 10:46:02 +0200

    Bump NGinx to 1.15.0
    Signed-off-by: Mathias Kaufmann <me@stei.gr>

--- images/nginx/build.sh
+++ images/nginx/build.sh
@@ -19,7 +19,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-export NGINX_VERSION=1.13.12
+export NGINX_VERSION=1.15.0
 export NDK_VERSION=0.3.0
 export VTS_VERSION=0.1.16
 export SETMISC_VERSION=0.31
@@ -114,7 +114,7 @@ mkdir --verbose -p "$BUILD_PATH"
 cd "$BUILD_PATH"
 
 # download, verify and extract the source files
-get_src fb92f5602cdb8d3ab1ad47dbeca151b185d62eedb67d347bbe9d79c1438c85de \
+get_src b0b58c9a3fd73aa8b89edf5cfadc6641a352e0e6d3071db1eb3215d72b7fb516 \
         "http://nginx.org/download/nginx-$NGINX_VERSION.tar.gz"
 
 get_src 88e05a99a8a7419066f5ae75966fb1efc409bad4522d14986da074554ae61619 \
